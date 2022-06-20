2f7e559a23c74647b424aacb38ecec2809678b7b
sharkz <sharkz@localhost.localdomain>
2022-06-20 15:19:32 -0400

    c

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,7 +1,7 @@
-import IMG1 from "../assets/portfolio1.jpg";
-import IMG2 from "../assets/portfolio2.jpg";
-import IMG3 from "../assets/portfolio3.jpg";
-import IMG4 from "../assets/portfolio4.jpg";
+import IMG1 from "../assets/mobile.png";
+import IMG2 from "../assets/mobile1.png";
+import IMG3 from "../assets/portfolio4.jpg";
+import IMG4 from "../assets/mobile2.png";
 import IMG5 from "../assets/portfolio5.png";
 import IMG6 from "../assets/portfolio6.jpg";
 import Article from "./PortArticle";
@@ -13,10 +13,10 @@ const Portifolio=()=>{
            <h2>Portifolio</h2>
 
            <div className="container portifolio__container">   
-           <Article image={IMG1} title="This is a title" github={'https://gitbub.com'} demo={''}/>         
-           <Article image={IMG2} title="This is a title" github={'https://gitbub.com'} demo={''}/>                  <Article image={IMG1} title="This is a title" github={'https://gitbub.com'} demo={''}/>         
-           <Article image={IMG3} title="This is a title" github={'https://gitbub.com'} demo={''}/>         
-           <Article image={IMG4} title="This is a title" github={'https://gitbub.com'} demo={''}/>         
+           <Article image={IMG1} title="E-Shopping 'SHARKZ' STORE'" github={'https://github.com/SHARKZTECH/e--biz'} demo={'https://e--biz.herokuapp.com/'}/>         
+           <Article image={IMG2} title="Js,HMTL,CSS RECIPE APP" github={'https://github.com/SHARKZTECH/JS-RECIPE'} demo={'https://sharkztech.github.io/JS-RECIPE/'}/>                           
+           <Article image={IMG3} title="React Covid App" github={'https://github.com/SHARKZTECH/covid'} demo={''}/>         
+           <Article image={IMG4} title="React Advice App" github={'https://github.com/SHARKZTECH/advicev2'} demo={'https://sharkztech.github.io/advicev2/'}/>         
            <Article image={IMG5} title="This is a title" github={'https://gitbub.com'} demo={''}/>         
            <Article image={IMG6} title="This is a title" github={'https://gitbub.com'} demo={''}/>      
            </div>
