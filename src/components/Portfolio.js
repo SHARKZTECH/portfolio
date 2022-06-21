5af509b0695506038b8d663c0b608452cf6c6050
sharkz <sharkz@localhost.localdomain>
2022-06-21 08:59:04 -0400

    c1

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,6 @@
 import IMG1 from "../assets/mobile.png";
 import IMG2 from "../assets/mobile1.png";
-import IMG3 from "../assets/portfolio4.jpg";
+import IMG3 from "../assets/mobile3.png";
 import IMG4 from "../assets/mobile2.png";
 import IMG5 from "../assets/portfolio5.png";
 import IMG6 from "../assets/portfolio6.jpg";
@@ -13,9 +13,9 @@ const Portifolio=()=>{
            <h2>Portifolio</h2>
 
            <div className="container portifolio__container">   
-           <Article image={IMG1} title="E-Shopping 'SHARKZ' STORE'" github={'https://github.com/SHARKZTECH/e--biz'} demo={'https://e--biz.herokuapp.com/'}/>         
+           <Article image={IMG1} title="E-Shopping 'SHARKZ' STORE'" github={'https://github.com/SHARKZTECH/E-BIZ'} demo={'https://e--biz.herokuapp.com/'}/>         
            <Article image={IMG2} title="Js,HMTL,CSS RECIPE APP" github={'https://github.com/SHARKZTECH/JS-RECIPE'} demo={'https://sharkztech.github.io/JS-RECIPE/'}/>                           
-           <Article image={IMG3} title="React Covid App" github={'https://github.com/SHARKZTECH/covid'} demo={''}/>         
+           <Article image={IMG3} title="React Recipe App" github={'https://github.com/SHARKZTECH/redux4-react-recpie-v2'} demo={'https://sharkztech.github.io/redux4-react-recpie-v2/'}/>         
            <Article image={IMG4} title="React Advice App" github={'https://github.com/SHARKZTECH/advicev2'} demo={'https://sharkztech.github.io/advicev2/'}/>         
            <Article image={IMG5} title="This is a title" github={'https://gitbub.com'} demo={''}/>         
            <Article image={IMG6} title="This is a title" github={'https://gitbub.com'} demo={''}/>      
